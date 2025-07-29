166310994d0e35a30255627a26b3458b65fe0a4d
Lisseth00 <lissethrivas1328@gmail.com>
2025-07-29 16:43:18 -0500

    Añadida la tabla libro

--- SQL.sql
+++ SQL.sql
@@ -4,3 +4,15 @@ CREATE TABLE Categoria (
     nombre TEXT NOT NULL,
     descripcion TEXT
 );
+
+-- Tabla Libro
+CREATE TABLE Libro (
+    id_libro INTEGER PRIMARY KEY,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    isbn TEXT UNIQUE NOT NULL,
+    id_categoria INTEGER NOT NULL,
+    cantidad_stock INTEGER DEFAULT 0,
+    precio REAL NOT NULL,
+    FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria)
+);
